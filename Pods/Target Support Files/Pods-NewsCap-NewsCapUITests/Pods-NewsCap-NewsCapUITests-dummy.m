10431421517045963c4597953447125bc9f93f7a
Christophe Mei <c.mei@live.fr>
2020-06-05 18:01:28 +0200

    Clean repo

--- Pods/Target Support Files/Pods-NewsCap-NewsCapUITests/Pods-NewsCap-NewsCapUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NewsCap_NewsCapUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NewsCap_NewsCapUITests
-@end
--- Pods/Target Support Files/Pods-NewsCap/Pods-NewsCap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NewsCap : NSObject
-@end
-@implementation PodsDummy_Pods_NewsCap
-@end
